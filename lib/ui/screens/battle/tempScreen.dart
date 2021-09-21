b3bb7d32a9cca2b0ff8fc2cf12fb6ec35f206b06
wrteam-priyansh <wrteam.priyansh@gmail.com>
2021-09-21 14:23:33 +0530

    added emojis

--- lib/ui/screens/battle/tempScreen.dart
+++ lib/ui/screens/battle/tempScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterquiz/ui/screens/battle/widgets/messageBoxContainer.dart';
+import 'package:flutterquiz/ui/screens/battle/widgets/messageContainer.dart';
 import 'package:flutterquiz/ui/widgets/customRoundedButton.dart';
 import 'package:flutterquiz/utils/constants.dart';
 import 'package:flutterquiz/utils/uiUtils.dart';
@@ -11,7 +12,39 @@ class TempScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Icon(CupertinoIcons.chat_bubble_2_fill)),
+      body: Center(
+        child: CustomPaint(
+          painter: MessageCustomPainter(triangleIsLeft: false, color: Theme.of(context).colorScheme.secondary),
+          child: Container(
+            height: 60,
+            width: MediaQuery.of(context).size.width * (0.25),
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class EshopCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint();
+    paint.color = Colors.red;
+    Path path = Path();
+
+    path.moveTo(size.width * (0.1), 0);
+    path.lineTo(size.width * (0.9), 0);
+    path.quadraticBezierTo(size.width, 0, size.width, size.height * (0.25));
+
+    path.lineTo(size.width, size.height);
+
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
